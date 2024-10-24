55fb397c4dc4c4f676ebf9ec56ab97c8118cdaab
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-10-24 10:20:55 +0600

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <section>
+      <h1 className='text-4xl font-medium text-rose-500'>Hello Nextflix</h1>
+    </section>
+  );
+};
+
+export default HomePage;
