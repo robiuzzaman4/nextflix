07f0fb9369cd1e917458f7e5fdddfdb95418a3bd
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-10-24 10:42:27 +0600

    feat: navbar design done

---
+++ src/components/block/navbar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Container from "@/components/block/container";
+import Link from "next/link";
+import { Button } from "../ui/button";
+
+const Navbar = () => {
+  return (
+    <nav className="fixed top-0 w-full bg-background/50 backdrop-blur-xl border-b h-16 flex items-center">
+      <Container className="flex items-center justify-between gap-6">
+        <Link
+          href="/"
+          className="text-xl font-medium tracking-tighter text-rose-600"
+        >
+          Nextflix
+        </Link>
+        <Button asChild size="sm">
+          <Link href="/watchlist">Watchlist</Link>
+        </Button>
+      </Container>
+    </nav>
+  );
+};
+
+export default Navbar;
