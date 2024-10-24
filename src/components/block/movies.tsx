9b89aeeca825f5c748050a1e686011c77fd7a93d
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-10-24 14:17:11 +0600

    feat: fetch popular movies data

--- src/components/block/movies.tsx
+++ src/components/block/movies.tsx
@@ -1,10 +1,27 @@
+"use client";
+
 import React from "react";
 import Container from "@/components/block/container";
+import { useQuery } from "@tanstack/react-query";
 
 const Movies = () => {
+  
+  const response = useQuery({
+    queryKey: ["movies"],
+    queryFn: async () => {
+      const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
+      const response = await fetch(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+      );
+      return await response.json();
+    },
+  });
+
+  console.log("movies", response?.data);
+  
   return (
     <Container>
-      <h1 className="text-2xl">Movies</h1>
+      <h1 className="text-2xl">Movies : {response?.data?.results?.length}</h1>
     </Container>
   );
 };
