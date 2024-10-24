08e270da3d53cc5da6788c94727ac394867f4907
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-10-24 10:30:31 +0600

    feat: shadcn setup done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const HomePage = () => {
   return (
     <section>
       <h1 className="text-4xl font-medium text-rose-500 tracking-tighter">Hello Nextflix</h1>
+      <Button>Hello Movie</Button>
     </section>
   );
 };
