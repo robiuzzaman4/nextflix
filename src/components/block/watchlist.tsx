c53e36e6728b292c55011a1346995b309df85804
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-10-26 11:58:39 +0600

    feat: show watchlisted movies on ui

--- src/components/block/watchlist.tsx
+++ src/components/block/watchlist.tsx
@@ -3,6 +3,7 @@
 import { Movie } from "@/types";
 import { getWatchlistedMovies } from "@/utils";
 import React, { useEffect, useState } from "react";
+import MovieCard from "@/components/block/movie-card";
 
 const Watchlist = () => {
   const [watchlistedMovies, setWatchlistedMovies] = useState<Movie[]>([]);
@@ -14,13 +15,21 @@ const Watchlist = () => {
   }, []);
 
   return (
-    <section className="space-y-0">
+    <section className="space-y-6">
       <h3 className="text-xl md:text-2xl lg:text-3xl font-medium tracking-tighter">
         Watchlist
       </h3>
       {watchlistedMovies?.length <= 0 && (
         <div className="w-fit mx-auto py-16">
           <h1 className="text-xl tracking-tighter">Your watchlist is empty!</h1>
+        </div>
+      )}
+
+      {watchlistedMovies?.length > 0 && (
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5 gap-6">
+          {watchlistedMovies?.map((movie, index) => (
+            <MovieCard key={index} movie={movie} />
+          ))}
         </div>
       )}
     </section>
