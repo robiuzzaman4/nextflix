bd38a685b6e103a7a3c23a66305824e7ee73f3ce
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-10-26 11:54:40 +0600

    feat: get watchlisted movies on watchlist page

--- src/components/block/watchlist.tsx
+++ src/components/block/watchlist.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+"use client";
+
+import { Movie } from "@/types";
+import { getWatchlistedMovies } from "@/utils";
+import React, { useEffect, useState } from "react";
 
 const Watchlist = () => {
-    return (
-        <div>
-              <h2>Hello</h2>
+  const [watchlistedMovies, setWatchlistedMovies] = useState<Movie[]>([]);
+
+  // get watchlisted movies
+  useEffect(() => {
+    const wishlists = getWatchlistedMovies();
+    setWatchlistedMovies(wishlists);
+  }, []);
+
+  return (
+    <section className="space-y-0">
+      <h3 className="text-xl md:text-2xl lg:text-3xl font-medium tracking-tighter">
+        Watchlist
+      </h3>
+      {watchlistedMovies?.length <= 0 && (
+        <div className="w-fit mx-auto py-16">
+          <h1 className="text-xl tracking-tighter">Your watchlist is empty!</h1>
         </div>
-    );
+      )}
+    </section>
+  );
 };
 
 export default Watchlist;
